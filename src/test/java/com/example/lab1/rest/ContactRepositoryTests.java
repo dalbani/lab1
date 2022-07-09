aab343142a4925a02080aec90cd2d4f197d705e7
Damiano Albani <damiano.albani@gmail.com>
2022-07-09 17:36:54 +0200

    Expand tests for ContactRepository (happy path)

--- src/test/java/com/example/lab1/rest/ContactRepositoryTests.java
+++ src/test/java/com/example/lab1/rest/ContactRepositoryTests.java
@@ -42,10 +42,10 @@ class ContactRepositoryTests {
     @Autowired
     private ContactRepository contactRepository;
 
+    private final AtomicLong createdContactId = new AtomicLong();
+
     @Test
     void whenCreateContact_thenSuccess() {
-        AtomicLong createdContactId = new AtomicLong();
-
         buildRequestSpecification()
                 .body(Contact.builder()
                         .name(NAME)
@@ -71,6 +71,64 @@ class ContactRepositoryTests {
         assertThat(contact.getZipCode()).isEqualTo(ZIP_CODE);
         assertThat(contact.getCity()).isEqualTo(CITY);
         assertThat(contact.getHouseNumber()).isEqualTo(HOUSE_NUMBER);
+
+        String jsonBasePath = String.format("_embedded.contacts[%d].", createdContactId.get() - 1);
+
+        buildRequestSpecification()
+                .get("/contacts")
+                .prettyPeek()
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .contentType(matches(contentType -> MediaType.parseMediaType(contentType).isCompatibleWith(MediaTypes.HAL_JSON)))
+                .body(jsonBasePath + "id", equalTo(Long.valueOf(createdContactId.get()).intValue()))
+                .body(jsonBasePath + "name", equalTo(NAME))
+                .body(jsonBasePath + "zipCode", equalTo(ZIP_CODE))
+                .body(jsonBasePath + "city", equalTo(CITY))
+                .body(jsonBasePath + "houseNumber", equalTo(HOUSE_NUMBER));
+    }
+
+    @Test
+    void whenModifyContact_thenSuccess() {
+        whenCreateContact_thenSuccess();
+
+        buildRequestSpecification()
+                .body(Contact.builder()
+                        .name(NAME.toUpperCase())
+                        .zipCode(ZIP_CODE)
+                        .city(CITY)
+                        .houseNumber(HOUSE_NUMBER)
+                        .build())
+                .contentType(ContentType.JSON)
+                .put("/contacts/" + createdContactId.get())
+                .prettyPeek()
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .contentType(matches(contentType -> MediaType.parseMediaType(contentType).isCompatibleWith(MediaTypes.HAL_JSON)))
+                .body("id", equalTo(Long.valueOf(createdContactId.get()).intValue()))
+                .body("name", equalTo(NAME.toUpperCase()))
+                .body("zipCode", equalTo(ZIP_CODE))
+                .body("city", equalTo(CITY))
+                .body("houseNumber", equalTo(HOUSE_NUMBER));
+
+        Contact contact = contactRepository.findById(createdContactId.get()).orElseThrow();
+        assertThat(contact.getId()).isEqualTo(createdContactId.get());
+        assertThat(contact.getName()).isEqualTo(NAME.toUpperCase());
+        assertThat(contact.getZipCode()).isEqualTo(ZIP_CODE);
+        assertThat(contact.getCity()).isEqualTo(CITY);
+        assertThat(contact.getHouseNumber()).isEqualTo(HOUSE_NUMBER);
+    }
+
+    @Test
+    void whenDeleteContact_thenSuccess() {
+        whenCreateContact_thenSuccess();
+
+        buildRequestSpecification()
+                .delete("/contacts/" + createdContactId.get())
+                .prettyPeek()
+                .then()
+                .statusCode(HttpStatus.NO_CONTENT.value());
+
+        assertThat(contactRepository.findById(createdContactId.get())).isEmpty();
     }
 
     private RequestSpecification buildRequestSpecification() {
